53bc85ab02dbed729fcbfeb03c57cbf38c62d6e9
Sam Chiu <samchiu064@gmail.com>
2023-05-22 22:58:28 +0800

    feat: 顧客端主選單及今日訂單頁切版

--- src/components/client/mainMenu/CheckoutModal.vue
+++ src/components/client/mainMenu/CheckoutModal.vue
@@ -1,16 +1,51 @@
 <template>
-  <div></div>
+  <div v-if="showModal" class="fixed inset-0 bg-primary-bg z-20">
+    <div class="flex justify-center items-center h-full w-full">
+      <div
+        class="flex flex-col items-center gap-y-4 p-12 w-[730px] bg-secondary-white rounded-3xl shadow-[0_4px_24px_rgba(0,0,0,0.1)]"
+      >
+        <p class="text-2xl font-bold">結帳請至櫃台</p>
+        <p class="flex items-center gap-x-4">
+          <span class="text-xl">今日消費金額為</span>
+          <span class="text-3.5xl font-black text-primary-blue">$ 1,430</span>
+        </p>
+        <p class="text-gray-9f text-base text-center">
+          我們的服務人員將會協助您完成結帳程序。<br />
+          再次感謝您的蒞臨，期待您的下次光臨！
+        </p>
+        <p class="flex items-center gap-x-6 mt-2">
+          <span>本店提供：</span>
+          <span
+            class="before:block before:absolute before:-inset-1 before:rounded-full before:bg-primary-blue relative inline-block px-4"
+          >
+            <span class="relative text-white">現金支付</span>
+          </span>
+        </p>
+        <div class="flex gap-x-4 w-full">
+          <button
+            type="button"
+            class="block w-full py-3 bg-primary-orange text-secondary-white mt-4 rounded-md"
+            @click="toFeedbackPage"
+          >
+            填寫意見回饋
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import modalMixin from '@/mixins/modalMixin'
 
 export default defineComponent({
-  data() {
-    return {}
-  },
-  methods: {}
+  inheritAttrs: false,
+  mixins: [modalMixin],
+  methods: {
+    toFeedbackPage() {
+      this.$router.push({ path: '/client/feedback' })
+    }
+  }
 })
 </script>
-
-<style scss scoped></style>
--- src/views/client/TodayOrders.vue
+++ src/views/client/TodayOrders.vue
@@ -1,5 +1,40 @@
 <template>
-  <main>今日訂單明細頁</main>
+  <div class="bg-primary-bg h-screen w-screen">
+    <header class="h-20 w-full bg-gray-33 px-12 py-2.5 flex items-center">
+      <button type="button" class="flex items-center gap-x-2 py-2 pe-2" @click="backToMainMenu">
+        <span class="material-icons-outlined text-secondary-white text-xl">chevron_left</span>
+        <span class="text-secondary-white">返回</span>
+      </button>
+    </header>
+    <div class="flex justify-between h-[calc(100%-80px)]">
+      <main class="px-12 py-4 w-full overflow-auto">
+        <table class="table-auto w-full text-center">
+          <thead>
+            <tr class="text-lg text-gray-9f">
+              <th class="text-start">餐點項目</th>
+              <th>數量</th>
+              <th>金額</th>
+              <th>狀態</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="align-top border-b">
+              <td class="text-start py-2">
+                <p>德式脆皮豬腳</p>
+                <p class="text-gray-9f text-sm">小辣 / 起司</p>
+              </td>
+              <td class="py-2">X 1</td>
+              <td class="py-2">$500</td>
+              <td class="py-2">已送達</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
+      <div
+        class="flex flex-col gap-y-4 bg-[url('@/assets/images/client/client-bg-sidebar.png')] bg-contain pt-4 px-5 min-w-[360px]"
+      ></div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +43,11 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {}
+  },
+  methods: {
+    backToMainMenu() {
+      this.$router.push({ path: '/client/main-menu' })
+    }
   }
 })
 </script>
