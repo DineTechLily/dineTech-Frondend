867d178a4d381199116ae3b4a077124a18a79893
Sam Chiu <samchiu064@gmail.com>
2023-06-05 22:25:14 +0800

    feat: 專案加入 PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,51 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(
-    {
+  plugins: [
+    vue({
       template: {
         compilerOptions: {
           isCustomElement: (tag) => ['swiper-container', 'swiper-slide'].includes(tag)
         }
       }
-    }
-  )],
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,jpg,png,svg}']
+      },
+      manifest: {
+        start_url: '/',
+        name: 'DineTech 餐飲 iPad POS 系統',
+        short_name: 'DineTech',
+        description: '專為小型餐飲業者設計的 iPad POS 點餐系統，功能涵蓋顧客點餐以及後臺管理功能。',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
